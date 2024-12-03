206a7adcc20393e915aea82f6632ea70a5459fee
Kamilla289 <kami.rivay56@gmail.com>
2024-12-03 23:24:47 +0500

    add: top menu navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import modal from './modules/modal'
 import accordeon from './modules/accordeon'
 import arrowUp from './modules/arrowUp'
+import topMenu from './modules/topMenu'
 
 
 modal()
 accordeon()
 arrowUp()
+topMenu()
---
+++ src/modules/topMenu.js
@@ -0,0 +1,18 @@
+const topMenu = () => {
+  const links = document.querySelectorAll('.top-menu a');
+    links.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const targetId = link.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      targetElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+      });
+    });
+  });
+}
+
+export default topMenu;
