6b86b389ec23b20c44a54f2b593ce2c6a4f624b1
Kamilla289 <kami.rivay56@gmail.com>
2024-12-04 00:59:38 +0500

    fix: slider top

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,50 +1,50 @@
 const slider = () => {
-  const slides = document.querySelectorAll(".top-slider .item");
-	const dotsContainer = document.createElement("ul");
-	let currentSlideIndex = 0;
-	const dots = document.querySelectorAll(".slick-dots li");
-
-  dotsContainer.classList.add("slick-dots");
-  
-	slides.forEach((slide, index) => {
-		const dot = document.createElement("li");
-		dot.dataset.index = index;
-		if (index === 0) dot.classList.add("slick-active");
-		dotsContainer.appendChild(dot);
-	});
-	
-	document.querySelector(".top-slider").appendChild(dotsContainer);
-
-	function changeSlide(index) {
-		slides.forEach((slide) => {
-		  slide.classList.remove("active");
+	document.addEventListener("DOMContentLoaded", function () {
+		const slides = document.querySelectorAll(".top-slider .item");
+		const dotsContainer = document.createElement("ul");
+		let currentSlideIndex = 0;
+		
+		dotsContainer.classList.add("slick-dots");
+		slides.forEach((slide, index) => {
+			const dot = document.createElement("li");
+			dot.dataset.index = index;
+			if (index === 0) dot.classList.add("slick-active");
+			dotsContainer.appendChild(dot);
 		});
 
-	  slides[index].classList.add("active");
+		document.querySelector(".top-slider").appendChild(dotsContainer);
 
-	
-	  dots.forEach((dot, i) => {
-		  if (i === index) {
-			dot.classList.add("slick-active");
-		  } else {
-			dot.classList.remove("slick-active");
-		  }
-	  });
-	}
+		function changeSlide(index) {
+			slides.forEach((slide) => {
+			slide.classList.remove("active");
+			});
 
-	setInterval(() => {
-		currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-		changeSlide(currentSlideIndex);
-	}, 3000);
+			slides[index].classList.add("active");
 
-	dotsContainer.addEventListener("click", (e) => {
-		if (e.target.tagName === "LI") {
-		  currentSlideIndex = parseInt(e.target.dataset.index);
-		  changeSlide(currentSlideIndex);
+			const dots = document.querySelectorAll(".slick-dots li");
+			dots.forEach((dot, i) => {
+			if (i === index) {
+				dot.classList.add("slick-active");
+			} else {
+				dot.classList.remove("slick-active");
+			}
+			});
 		}
-	});
 
-	changeSlide(currentSlideIndex);
+		setInterval(() => {
+			currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+			changeSlide(currentSlideIndex);
+		}, 3000);
+
+		dotsContainer.addEventListener("click", (e) => {
+			if (e.target.tagName === "LI") {
+			currentSlideIndex = parseInt(e.target.dataset.index);
+			changeSlide(currentSlideIndex);
+			}
+		});
+
+		changeSlide(currentSlideIndex);
+		}); 
 }
 
 export default slider;
