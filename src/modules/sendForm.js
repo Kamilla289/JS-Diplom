dde0399337108434d365343735821c1386381fba
Kamilla289 <kami.rivay56@gmail.com>
2024-12-05 17:46:49 +0500

    fix: time message and color

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -21,10 +21,12 @@ const sendForm = () => {
       createStatusMessage();
       if (!/^\+?\d+$/.test(phoneValue)) {
           statusMessage.textContent = 'Телефон может содержать только цифры и знак "+"';
+          statusMessage.style.color = 'red';
           return;
       }
       if (!/^[а-яА-ЯёЁ\s]+$/.test(nameValue)) {
           statusMessage.textContent = 'Имя может содержать только кириллицу';
+          statusMessage.style.color = 'red';
           return;
       }
       statusMessage.textContent = 'Отправка данных...';
@@ -47,14 +49,19 @@ const sendForm = () => {
               statusMessage.textContent = 'Ошибка при отправке данных.';
               statusMessage.style.color = 'red';
           }
+          setTimeout(() => {
+            statusMessage.textContent = '';
+          }, 5000);
       })
       .catch(error => {
-          console.error('Ошибка при отправке:', error);
-          statusMessage.textContent = 'Произошла ошибка при отправке данных.';
-          statusMessage.style.color = 'red';
+        console.error('Ошибка при отправке:', error);
+        statusMessage.textContent = 'Произошла ошибка при отправке данных.';
+        statusMessage.style.color = 'red';
+        setTimeout(() => {
+            statusMessage.textContent = '';
+        }, 5000);
       });
   });
-
 }
 
 export default sendForm;
